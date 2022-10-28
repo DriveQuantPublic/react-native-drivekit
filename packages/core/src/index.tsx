53272ab7b459964b40e304198ec9a8d0a4ae3e46
Clément Taboulot <clement-taboulot@hotmail.fr>
2022-10-28 18:18:58 +0200

    :sparkles: (core) Initialize core package

---
+++ packages/core/src/index.tsx
@@ -0,0 +1,29 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package '@react-native-drivekit/core' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+// @ts-expect-error
+const isTurboModuleEnabled = global.__turboModuleProxy != null;
+
+const CoreModule = isTurboModuleEnabled
+  ? require('./NativeCore').default
+  : NativeModules.Core;
+
+const Core = CoreModule
+  ? CoreModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Core.multiply(a, b);
+}
