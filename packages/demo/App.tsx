5f21783f56a175f1d6bce81647f43faf61c09cc8
Clément Taboulot <clement-taboulot@hotmail.fr>
2022-10-31 15:36:27 +0100

    :sparkles: (demo) Ask bluetooth permission for iOS

--- packages/demo/App.tsx
+++ packages/demo/App.tsx
@@ -1,18 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, useColorScheme} from 'react-native';
-
+import {
+  Alert,
+  Platform,
+  SafeAreaView,
+  Text,
+  useColorScheme,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {multiply} from '@react-native-drivekit/core';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+const checkBluetoothPermissions = async () => {
+  if (Platform.OS !== 'ios') {
+    return;
+  }
+
+  const permission = await check(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
+  switch (permission) {
+    case RESULTS.UNAVAILABLE:
+      Alert.alert('Bluetooth not available on this device');
+      break;
+    case RESULTS.DENIED:
+      await request(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
+      break;
+    case RESULTS.LIMITED:
+      Alert.alert('The permission is limited: some actions are possible');
+      break;
+    case RESULTS.GRANTED:
+      break;
+    case RESULTS.BLOCKED:
+      Alert.alert(
+        'The permission is denied and not requestable anymore. You need to go in the app settings',
+      );
+      break;
+  }
+};
 
 const App = () => {
   const [result, setResult] = useState(0);
@@ -22,7 +44,9 @@ const App = () => {
     async function calculate() {
       setResult(await multiply(2, 3));
     }
+
     calculate();
+    checkBluetoothPermissions();
   }, []);
 
   const backgroundStyle = {
