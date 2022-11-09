97512c281401933ec277339725bfa79b2835df70
Clément Taboulot <clement-taboulot@hotmail.fr>
2022-11-09 14:13:41 +0100

    :robot: (demo) Add android bluetooth permission

--- packages/demo/src/services/permissions/bluetooth.ts
+++ packages/demo/src/services/permissions/bluetooth.ts
@@ -1,29 +1,64 @@
-import {Alert, Platform} from 'react-native';
-import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {Alert, Linking, Platform} from 'react-native';
+import {
+  check,
+  request,
+  PERMISSIONS,
+  RESULTS,
+  Permission,
+} from 'react-native-permissions';
+
+const IOS_PERMISSIONS = [PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL];
+
+const ANDROID_PERMISSIONS = [
+  PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+  PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+];
 
 const checkBluetoothPermissions = async () => {
-  if (Platform.OS !== 'ios') {
-    return;
+  if (Platform.OS === 'ios') {
+    await checkPermissions(IOS_PERMISSIONS);
+  } else {
+    await checkPermissions(ANDROID_PERMISSIONS);
   }
+};
 
-  const permission = await check(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
-  switch (permission) {
-    case RESULTS.UNAVAILABLE:
-      Alert.alert('Bluetooth not available on this device');
-      break;
-    case RESULTS.DENIED:
-      await request(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
-      break;
-    case RESULTS.LIMITED:
-      Alert.alert('The permission is limited: some actions are possible');
-      break;
-    case RESULTS.GRANTED:
-      break;
-    case RESULTS.BLOCKED:
-      Alert.alert(
-        'The permission is denied and not requestable anymore. You need to go in the app settings',
-      );
-      break;
+const checkPermissions = async (locationPermission: Permission[]) => {
+  for (let index = 0; index < locationPermission.length; index++) {
+    const permission = locationPermission[index];
+
+    const permissionStatus = await check(permission);
+
+    switch (permissionStatus) {
+      case RESULTS.UNAVAILABLE:
+        break;
+      case RESULTS.DENIED:
+        await request(permission);
+        break;
+      case RESULTS.LIMITED:
+        Alert.alert(
+          'Please accept bluetooth permission in your settings.',
+          undefined,
+          [
+            {
+              onPress: () => Linking.openSettings(),
+            },
+          ],
+        );
+        break;
+      case RESULTS.GRANTED:
+        break;
+      case RESULTS.BLOCKED:
+        Alert.alert(
+          'The bluetooth is denied and not requestable anymore. You need to go in the app settings',
+          undefined,
+          [
+            {
+              onPress: () => Linking.openSettings(),
+            },
+          ],
+        );
+        break;
+    }
   }
 };
 
