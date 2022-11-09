6be9ab4a8da90b9a9dfcf013ced2fd0911b89564
Cyril Bonaccini <cyrilb@bam.tech>
2022-11-09 12:08:08 +0100

    prune: remove UI associated to sandbox mode

--- packages/demo/App.tsx
+++ packages/demo/App.tsx
@@ -24,7 +24,6 @@ const App = () => {
   const [userId, setUserId] = useState('');
   const [newUserId, setNewUserId] = useState('');
   const [instantDeleteAccount, setInstantDeleteAccount] = useState(false);
-  const [isSandboxMode, setIsSandboxMode] = useState(false);
 
   const [result, setResult] = useState(0);
 
@@ -114,18 +113,6 @@ const App = () => {
           onPress={async () => {
             const isTokenValid = await DriveKit.isTokenValid();
             Alert.alert(isTokenValid ? 'Token is valid' : 'Token is not valid');
-          }}
-        />
-        <Spacer factor={2} />
-        <Text style={styles.title}>Sandbox Mode</Text>
-        <Spacer factor={1} />
-        <Button
-          title={
-            isSandboxMode ? 'Enable production mode' : 'Enable sandbox mode'
-          }
-          onPress={async () => {
-            DriveKit.enableSandboxMode(!isSandboxMode);
-            setIsSandboxMode(prevValue => !prevValue);
           }}
         />
       </ScrollView>
