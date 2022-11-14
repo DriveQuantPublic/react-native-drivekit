4ace51df3dd8fe89f1f9e9cb087878611ac79b53
Cyril Bonaccini <cyrilb@bam.tech>
2022-11-14 17:32:34 +0100

    feat: add potentialTripStart on android

--- packages/trip-analysis/android/src/main/java/com/reactnativedrivekittripanalysis/DrivekitTripAnalysisModule.kt
+++ packages/trip-analysis/android/src/main/java/com/reactnativedrivekittripanalysis/DrivekitTripAnalysisModule.kt
@@ -1,14 +1,39 @@
 package com.reactnativedrivekittripanalysis
+import com.drivequant.drivekit.tripanalysis.DeviceConfigEvent
 import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysis
+import com.drivequant.drivekit.tripanalysis.TripListener
+import com.drivequant.drivekit.tripanalysis.entity.PostGeneric
+import com.drivequant.drivekit.tripanalysis.entity.PostGenericResponse
 import com.drivequant.drivekit.tripanalysis.entity.TripNotification
+import com.drivequant.drivekit.tripanalysis.entity.TripPoint
+import com.drivequant.drivekit.tripanalysis.model.crashdetection.DKCrashInfo
+import com.drivequant.drivekit.tripanalysis.service.crashdetection.feedback.CrashFeedbackSeverity
+import com.drivequant.drivekit.tripanalysis.service.crashdetection.feedback.CrashFeedbackType
+import com.drivequant.drivekit.tripanalysis.service.recorder.StartMode
+import com.drivequant.drivekit.tripanalysis.service.recorder.State
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class DrivekitTripAnalysisModule internal constructor(context: ReactApplicationContext) :
   DrivekitTripAnalysisSpec(context) {
 
+  init {
+    reactContext = context;
+  }
+
   override fun getName(): String {
     return NAME
+  }
+
+  @ReactMethod
+  fun addListener(eventName: String) {
+    // Set up any upstream listeners or background tasks as necessary
+  }
+
+  @ReactMethod
+  fun removeListeners(count: Int) {
+    // Remove upstream listeners, stop unnecessary background tasks
   }
 
   @ReactMethod
@@ -33,9 +58,40 @@ class DrivekitTripAnalysisModule internal constructor(context: ReactApplicationC
 
   companion object {
     const val NAME = "RNDriveKitTripAnalysis"
-
+    private var reactContext: ReactApplicationContext? = null;
     fun initialize(iconId: Int) {
-      DriveKitTripAnalysis.initialize(createForegroundNotification(iconId))
+      DriveKitTripAnalysis.initialize(createForegroundNotification(iconId), object: TripListener {
+        override fun tripStarted(startMode : StartMode) {
+        }
+        override fun tripPoint(tripPoint : TripPoint) {
+        }
+        override fun tripSavedForRepost() {
+        }
+        override fun tripFinished(post : PostGeneric, response: PostGenericResponse) {
+        }
+        override fun beaconDetected() {
+        }
+        override fun sdkStateChanged(state: State) {
+        }
+        override fun potentialTripStart(startMode: StartMode) {
+          var eventName = when (startMode) {
+            StartMode.UNKNOWN_BLUETOOTH -> "UNKNOWN_BLUETOOTH"
+            StartMode.BEACON -> "BEACON"
+            StartMode.BICYCLE_ACTIVITY -> "BICYCLE_ACTIVITY"
+            StartMode.BLUETOOTH -> "BLUETOOTH"
+            StartMode.GEOZONE -> "GEOZONE"
+            StartMode.GPS -> "GPS"
+            StartMode.MANUAL -> "MANUAL"
+          }
+          reactContext?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)?.emit("potentialTripStart", eventName)
+        }
+        override fun crashDetected(crashInfo: DKCrashInfo) {
+        }
+        override fun crashFeedbackSent(crashInfo: DKCrashInfo, feedbackType: CrashFeedbackType, severity: CrashFeedbackSeverity) {
+        }
+        override fun onDeviceConfigEvent(deviceConfigevent: DeviceConfigEvent) {
+        }
+      })
     }
 
     private fun createForegroundNotification(iconId: Int): TripNotification{
