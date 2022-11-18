5e663cc3f82b7d5c6245d208101638ed6ae875cf
Cyril Bonaccini <cyrilb@bam.tech>
2022-11-18 14:24:23 +0100

    refactor: split index.ts into api and types files

---
+++ packages/trip-analysis/src/types/trip.ts
@@ -0,0 +1,32 @@
+export enum CancelTripReason {
+  USER = 'USER',
+  HIGH_SPEED = 'HIGH_SPEED',
+  NO_SPEED = 'NO_SPEED',
+  NO_BEACON = 'NO_BEACON',
+  MISSING_CONFIGURATION = 'MISSING_CONFIGURATION',
+  NO_GPS_DATA = 'NO_GPS_DATA',
+  RESET = 'RESET',
+  BEACON_NO_SPEED = 'BEACON_NO_SPEED',
+  NO_BLUETOOTH_DEVICE = 'NO_BLUETOOTH_DEVICE',
+}
+
+export enum StartMode {
+  GPS = 'GPS',
+  BEACON = 'BEACON',
+  MANUAL = 'MANUAL',
+  GEOZONE = 'GEOZONE',
+  BLUETOOTH = 'BLUETOOTH',
+  BLUETOOTH_UNKNOWN = 'BLUETOOTH_UNKNOWN',
+  BICYCLE_ACTIVITY = 'BICYCLE_ACTIVITY',
+}
+
+export type TripPoint = {
+  latitude: number;
+  longitude: number;
+  speed: number;
+  accuracy: number;
+  elevation: number;
+  distance: number;
+  heading: number;
+  duration: number;
+};
