037cf1c25849264f5798fd5d77d1edc17c45e87c
Cyril Bonaccini <cyrilb@bam.tech>
2022-11-18 18:40:10 +0100

    fix: type imports in App.tsx

--- packages/core/src/api.tsx
+++ packages/core/src/api.tsx
@@ -10,11 +10,13 @@ import type {
   RequestError,
   UpdateUserIdStatus,
 } from './types';
+
 const LINKING_ERROR =
   `The package '@react-native-drivekit/core' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
+
 // @ts-expect-error
 const isTurboModuleEnabled = global.__turboModuleProxy != null;
 
