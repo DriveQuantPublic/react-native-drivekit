840a7b2f4b180cf62090142f6348f77e63b14d14
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2023-06-15 17:22:45 +0200

    chore: fix TripVehicle object creation from a react native map

--- packages/trip-analysis/android/src/main/java/com/reactnativedrivekittripanalysis/VehicleMappers.kt
+++ packages/trip-analysis/android/src/main/java/com/reactnativedrivekittripanalysis/VehicleMappers.kt
@@ -1,69 +1,24 @@
 package com.reactnativedrivekittripanalysis
 
-import com.drivequant.drivekit.databaseutils.entity.TripVehicle
+import com.drivequant.drivekit.tripanalysis.entity.TripVehicle
 import com.facebook.react.bridge.ReadableMap
 
 fun mapReadableMapToVehicle(vehicle: ReadableMap): TripVehicle {
-  var result = TripVehicle();
-  if (vehicle.hasKey("carTypeIndex")) {
-    result.typeIndex = vehicle.getInt("carTypeIndex");
-  }
-
-  if (vehicle.hasKey("carEngineIndex")) {
-    result.engineIndex = vehicle.getInt("carEngineIndex");
-  }
-
-  if (vehicle.hasKey("carPower")) {
-    result.power = vehicle.getDouble("carPower");
-  }
-
-  if (vehicle.hasKey("carMass")) {
-    result.mass = vehicle.getDouble("carMass");
-  }
-
-  if (vehicle.hasKey("carGearboxIndex")) {
-    result.gearboxIndex = vehicle.getInt("carGearboxIndex");
-  }
-
-  if (vehicle.hasKey("carConsumption")) {
-    result.consumption = vehicle.getDouble("carConsumption");
-  }
-
-  if (vehicle.hasKey("carAutoGearboxNumber")) {
-    result.autoGearboxNumber = vehicle.getInt("carAutoGearboxNumber");
-  }
-
-  if (vehicle.hasKey("engineDisplacement")) {
-    result.engineDisplacement = vehicle.getDouble("engineDisplacement");
-  }
-
-  if (vehicle.hasKey("frontTireSize")) {
-    result.frontTireSize = vehicle.getString("frontTireSize");
-  }
-
-  if (vehicle.hasKey("rearTireSize")) {
-    result.rearTireSize = vehicle.getString("rearTireSize");
-  }
-
-  if (vehicle.hasKey("length")) {
-    result.length = vehicle.getDouble("length");
-  }
-
-  if (vehicle.hasKey("width")) {
-    result.width = vehicle.getDouble("width");
-  }
-
-  if (vehicle.hasKey("height")) {
-    result.height = vehicle.getDouble("height");
-  }
-
-  if (vehicle.hasKey("engineCylinderNb")) {
-    result.engineCylinderNb = vehicle.getInt("engineCylinderNb");
-  }
-
-  if (vehicle.hasKey("driveWheels")) {
-    result.driveWheels = vehicle.getInt("driveWheels");
-  }
-
-  return result;
+  return TripVehicle(
+    carTypeIndex = vehicle.getInt("carTypeIndex"),
+    carEngineIndex = vehicle.getInt("carEngineIndex"),
+    carPower = vehicle.getDouble("carPower"),
+    carMass = vehicle.getDouble("carMass"),
+    carGearboxIndex = vehicle.getInt("carGearboxIndex"),
+    carConsumption = vehicle.getDouble("carConsumption"),
+    carAutoGearboxNumber = vehicle.getInt("carAutoGearboxNumber"),
+    engineDisplacement = vehicle.getDouble("engineDisplacement"),
+    frontTireSize = vehicle.getString("frontTireSize"),
+    rearTireSize = vehicle.getString("rearTireSize"),
+    length = vehicle.getDouble("length"),
+    width = vehicle.getDouble("width"),
+    height = vehicle.getDouble("height"),
+    engineCylinderNb = vehicle.getInt("engineCylinderNb"),
+    driveWheels = vehicle.getInt("driveWheels")
+  );
 }
