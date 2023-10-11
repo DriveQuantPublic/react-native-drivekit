25794d49b26b954e52a1821fd8bbb18634b02445
Steven <steven@drivequant.com>
2023-10-11 18:17:39 +0200

    feat: migrate projects to RN 0.71 for Android part

--- packages/demo/android/app/src/main/java/com/drivekit/drivekitrndemoapp/MainApplication.java
+++ packages/demo/android/app/src/main/java/com/drivekit/drivekitrndemoapp/MainApplication.java
@@ -1,107 +1,85 @@
 package com.drivekit.drivekitrndemoapp;
 
 import android.app.Application;
-import android.content.Context;
 
+import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.reactnativedrivekit.driverdata.DriveKitDriverDataModule;
 import com.reactnativedrivekitcore.DriveKitCoreModule;
 import com.reactnativedrivekittripanalysis.DriveKitTripAnalysisModule;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import com.drivekit.drivekitrndemoapp.newarchitecture.MainApplicationReactNativeHost;
 import com.reactnativedrivekittripanalysis.RNHeadlessJSNotification;
 import com.reactnativedrivekittripanalysis.RNTripNotification;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+            return BuildConfig.DEBUG;
         }
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for
+            // example:
+            // packages.add(new MyReactNativePackage());
+            return packages;
         }
 
         @Override
         protected String getJSMainModuleName() {
-          return "index";
+            return "index";
         }
-      };
 
-  private final ReactNativeHost mNewArchitectureNativeHost =
-      new MainApplicationReactNativeHost(this);
+        @Override
+        protected boolean isNewArchEnabled() {
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
-      return mReactNativeHost;
+        @Override
+        protected Boolean isHermesEnabled() {
+            return BuildConfig.IS_HERMES_ENABLED;
+        }
+    };
+
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
-  }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    DriveKitCoreModule.Companion.initialize(this);
-    final RNTripNotification tripNotification = new RNTripNotification(
-            "DriveKit SDK",
-            "Start a trip with DriveKit SDK",
-            R.drawable.common_google_signin_btn_icon_dark);
-    final RNHeadlessJSNotification headlessJSNotification = new RNHeadlessJSNotification("DriveKit SDK", "Loading in progress…");
-    DriveKitTripAnalysisModule.Companion.initialize(tripNotification, headlessJSNotification);
-    DriveKitDriverDataModule.Companion.initialize();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DriveKitCoreModule.Companion.initialize(this);
+        final RNTripNotification tripNotification = new RNTripNotification(
+                "DriveKit SDK",
+                "Start a trip with DriveKit SDK",
+                R.drawable.common_google_signin_btn_icon_dark);
+        final RNHeadlessJSNotification headlessJSNotification = new RNHeadlessJSNotification("DriveKit SDK",
+                "Loading in progress…");
+        DriveKitTripAnalysisModule.Companion.initialize(tripNotification, headlessJSNotification);
+        DriveKitDriverDataModule.Companion.initialize();
 
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-    SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-  }
+        // If you opted-in for the New Architecture, we enable the TurboModule system
+        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        SoLoader.init(this, /* native exopackage */ false);
 
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.drivekit.drivekitrndemoapp.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for
+            // this app.
+            DefaultNewArchitectureEntryPoint.load();
+        }
+        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
-  }
 }
