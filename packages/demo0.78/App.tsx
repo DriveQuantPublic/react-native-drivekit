6499b03f4fe6f0df6a62a89eea2057069244ed67
Lucas ROURET <lucas.rouretperso@gmail.com>
2025-04-10 12:05:09 +0200

    fix: RN permission en RN 0.78

--- packages/demo0.78/App.tsx
+++ packages/demo0.78/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { ApiKeySection } from './src/components/Sections/ApiKeySection';
-import { TokenSection } from './src/components/Sections/TokenSection';
-import { UpdateUserSection } from './src/components/Sections/UpdateUserSection';
-import { UserSection } from './src/components/Sections/UserSection';
-import { margins } from './src/margins';
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {ApiKeySection} from './src/components/Sections/ApiKeySection';
+import {TokenSection} from './src/components/Sections/TokenSection';
+import {UpdateUserSection} from './src/components/Sections/UpdateUserSection';
+import {UserSection} from './src/components/Sections/UserSection';
+import {margins} from './src/margins';
+import {useCheckPermissions} from './src/hooks/useCheckPermissions';
+import {DeleteAccountSection} from './src/components/Sections/DeleteAccountSection';
+import {LogsSection} from './src/components/Sections/LogsSection';
+import {ResetSection} from './src/components/Sections/ResetSection';
+import {TripSection} from './src/components/Sections/TripSection';
 
 const App = () => {
-/*   useCheckPermissions();
-  useSetupListeners(); */
+  useCheckPermissions();
+  // DriverData not available currently
+  // useSetupListeners();
 
   return (
     <SafeAreaView style={styles.page}>
@@ -18,7 +24,11 @@ const App = () => {
         <UserSection />
         <TokenSection />
         <UpdateUserSection />
-       
+
+        <TripSection />
+        <LogsSection />
+        <DeleteAccountSection />
+        <ResetSection />
       </ScrollView>
     </SafeAreaView>
   );
