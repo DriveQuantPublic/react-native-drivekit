4a2daad25ed115591e23545ef3a467b7bf7d47e4
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2025-05-20 17:51:31 +0200

    chore: fix core event emitter implementation for iOS

--- packages/core/ios/RNCoreEventEmitter.swift
+++ packages/core/ios/RNCoreEventEmitter.swift
@@ -14,7 +14,24 @@ class RNCoreEventEmitter: NSObject {
         RNCoreEventEmitter.eventEmitter = eventEmitter
     }
     @objc func dispatch(name: String, body: Any?) {
-        RNCoreEventEmitter.eventEmitter?.sendEvent(withName: name, body: body)
+      var selectorObj: Selector?
+      if name == "driveKitConnected" {
+        selectorObj = NSSelectorFromString("emitOnDriveKitConnected" )
+      } else if name == "driveKitDisconnected" {
+        selectorObj = NSSelectorFromString("emitOnDriveKitDisconnected" )
+      } else if name == "driveKitDidReceiveAuthenticationError" {
+        selectorObj = NSSelectorFromString("emitOnDriveKitDidReceiveAuthenticationError:" )
+      } else if name == "userIdUpdateStatusChanged" {
+        selectorObj = NSSelectorFromString("emitOnUserIdUpdateStatusChanged:" )
+      } else if name == "accountDeletionCompleted" {
+        selectorObj = NSSelectorFromString("emitOnAccountDeletionCompleted:" )
+      } else if name == "deviceConfigurationChanged" {
+        selectorObj = NSSelectorFromString("emitOnDeviceConfigurationChanged:" )
+      }
+
+      if let selectorObj, RNCoreEventEmitter.eventEmitter.responds(to: selectorObj) {
+        RNCoreEventEmitter.eventEmitter.perform(selectorObj, with: body)
+      }
     }
 
     @objc static var allEvents: [String] =  [
