1c4981e5c9868472b59e05aa34e9331e5c428c03
guillaumegremillet <guillaume.gremillet@drivequant.com>
2026-02-17 15:27:14 +0100

    feat: style permissions section

--- packages/demo-expo/app/components/PermissionsSection.tsx
+++ packages/demo-expo/app/components/PermissionsSection.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@react-navigation/elements";
 import { OpenOptimizationSettings } from "react-native-battery-optimization-check";
 import {
+  checkMultiple,
+  Permission,
   PERMISSIONS,
+  PermissionStatus,
   request,
   requestMultiple
 } from "react-native-permissions";
 
-import { Platform } from "react-native";
+import { useEffect, useState } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { SectionContainer } from "./SectionContainer";
 
 const IOS_PERMISSIONS = [
@@ -23,28 +27,53 @@ const ANDROID_PERMISSIONS = [
   PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
 ];
 
+const PermissionStatusEmoji: Record<PermissionStatus, string> = {
+  "unavailable": "❌",
+  "denied": "🚫",
+  "granted": "✅",
+  "limited": "⚠️",
+  "blocked": "⛔️",
+};
+
 export const PermissionsSection = () => {
+
+  const [permissionsStatus, setPermissionsStatus] = useState<Record<Permission[number], PermissionStatus>>({});
+
   const onPermissionsRequest = () => {
     if (Platform.OS === "ios") {
-      requestMultiple(IOS_PERMISSIONS).then((statuses) => {
-        console.log(statuses);
-      });
+      requestMultiple(IOS_PERMISSIONS)
+        .then(setPermissionsStatus);
     } else {
-      requestMultiple(ANDROID_PERMISSIONS).then((statuses) => {
-        console.log(statuses);
-        request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(
-          (status) => {
-            console.log(status);
-            OpenOptimizationSettings();
-          },
-        );
-      });
+      requestMultiple(ANDROID_PERMISSIONS)
+        .then(setPermissionsStatus)
+        .then(() => {
+          request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(
+            (status) => {
+              console.log(status);
+              OpenOptimizationSettings();
+            },
+          );
+        });
     }
   }
 
-
+  useEffect(() => {
+    if (Platform.OS === "ios") {
+      checkMultiple(IOS_PERMISSIONS)
+        .then(setPermissionsStatus);
+    } else {
+      checkMultiple(ANDROID_PERMISSIONS)
+        .then(setPermissionsStatus);
+    }
+  }, [])
 
   return <SectionContainer title="Permissions">
+    {Object.entries(permissionsStatus).map(([permission, status]) => (
+      <View key={permission} style={styles.permissionLine}>
+        <Text>{permission.split('.').pop()}</Text>
+        <Text>{PermissionStatusEmoji[status]} {status}</Text>
+      </View>
+    ))}
     <Button
       onPress={onPermissionsRequest}
     >
@@ -52,3 +81,10 @@ export const PermissionsSection = () => {
     </Button>
   </SectionContainer>
 }
+
+const styles = StyleSheet.create({
+  permissionLine: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  }
+})
