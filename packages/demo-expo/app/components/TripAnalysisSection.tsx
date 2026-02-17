a2339ffdaf9820559fd6c2ba17f7a9d010cedad4
guillaumegremillet <guillaume.gremillet@drivequant.com>
2026-02-17 16:13:57 +0100

    feat: add eventsListeners to sdk

--- packages/demo-expo/app/components/TripAnalysisSection.tsx
+++ packages/demo-expo/app/components/TripAnalysisSection.tsx
@@ -1,40 +1,100 @@
 import * as DriveKitTripAnalysis from "@react-native-drivekit/trip-analysis";
 
 import { Button } from "@react-navigation/elements";
-import { Alert } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { SectionContainer } from "./SectionContainer";
+import { useCallback, useEffect, useState } from "react";
+
+const events = [
+  "tripRecordingStarted",
+  "tripRecordingConfirmed",
+  "tripRecordingCanceled",
+  "tripRecordingFinished",
+  "tripFinishedWithResult",
+  "potentialTripStart",
+  "tripPoint",
+  "tripSavedForRepost",
+  "beaconDetected",
+  "significantLocationChangeDetected",
+  "sdkStateChanged",
+  "crashDetected",
+  "crashFeedbackSent"
+]
 
 export const TripAnalysisSection = () => {
-    return <SectionContainer title="Trip Analysis">
-            
-                  <Button
-                    onPress={() => {
-                      DriveKitTripAnalysis.activateAutoStart(true);
-                    }}
-                  >
-                    Activate Autostart
-                    </Button>
-                    <Button
-                      onPress={() => {
-                        DriveKitTripAnalysis.startTrip();
-                      }}
-                    >
-                      Start Trip
-                    </Button>
-                    <Button
-                      onPress={() => {
-                        DriveKitTripAnalysis.stopTrip();
-                      }}
-                    >
-                      Stop Trip
-                    </Button>
-                    <Button
-                      onPress={async () => {
-                        const result = await DriveKitTripAnalysis.isTripRunning();
-                        Alert.alert(result ? "Trip is running" : "Trip is not running");
-                      }}
-                    >
-                      Check Trip Running ?
-                    </Button>
-                  </SectionContainer>
+
+  const [lastReceivedEvent, setLastReceivedEvent] = useState<Array<string>>(Array<string>(5).fill(""));
+
+  const onNewEvent = useCallback((eventName: string, data?: any) => {
+    setLastReceivedEvent(prev => {
+      const newEvent = `${eventName} - ${data ? `: ${JSON.stringify(data)}` : ""}`;
+      const newEvents = [newEvent, ...prev];
+      if (newEvents.length > 5) {
+        newEvents.pop();
+      }
+      return newEvents;
+    })
+  }, [lastReceivedEvent])
+
+  useEffect(() => {
+    const listeners = events.map(event =>
+      DriveKitTripAnalysis.addEventListener(event, (data: any) => {
+        onNewEvent(event, data);
+      })
+    )
+
+    return () => {
+      listeners.forEach(listener => {
+        listener.remove();
+      })
+    }
+  }, [])
+
+  return <SectionContainer title="Trip Analysis">
+
+    <Text>Last 5 received event:</Text>
+    <View style={styles.eventsContainer}>
+      {lastReceivedEvent.map((event, index) => (
+        <Text key={index} numberOfLines={1}>{event}</Text>
+      ))}
+    </View>
+
+    <Button
+      onPress={() => {
+        DriveKitTripAnalysis.activateAutoStart(true);
+      }}
+    >
+      Activate Autostart
+    </Button>
+    <Button
+      onPress={() => {
+        DriveKitTripAnalysis.startTrip();
+      }}
+    >
+      Start Trip
+    </Button>
+    <Button
+      onPress={() => {
+        DriveKitTripAnalysis.stopTrip();
+      }}
+    >
+      Stop Trip
+    </Button>
+    <Button
+      onPress={async () => {
+        const result = await DriveKitTripAnalysis.isTripRunning();
+        Alert.alert(result ? "Trip is running" : "Trip is not running");
+      }}
+    >
+      Check Trip Running ?
+    </Button>
+  </SectionContainer>
 }
+
+const styles = StyleSheet.create({
+  eventsContainer: {
+    padding: 10,
+    backgroundColor: "#e0e0e0",
+    gap: 5,
+  },
+})
